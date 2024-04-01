d798f01d7b103ec23cf6cd8cebf7ce325cc605e8
Samiullah <samiullahtufail181@gmail.com>
2024-04-01 14:31:10 +0500

    first commit

---
+++ Index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let result = Math.floor(Math.random() * 11);
+// console.log(result);
+let answers = await inquirer.prompt([
+    {
+        name: "number",
+        message: "Enter your number",
+        type: "number",
+    },
+]);
+if (result === answers.number) {
+    console.log("Congragulation , you win");
+}
+else {
+    console.log("Please Try Agian");
+}
