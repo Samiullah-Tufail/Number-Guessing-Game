b8f69e28a4727628f207d455acd2e4cceb035962
Samiullah <samiullahtufail181@gmail.com>
2024-04-03 13:00:21 +0500

    second commit

--- Index.js
+++ Index.js
@@ -1,17 +1,20 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 let result = Math.floor(Math.random() * 11);
+let playagian = true;
 // console.log(result);
+console.log("-------------------- welcome to number gussing game --------------------");
 let answers = await inquirer.prompt([
     {
         name: "number",
-        message: "Enter your number",
+        message: "Enter uor number to guess, number range is 1 to 10:",
         type: "number",
     },
 ]);
 if (result === answers.number) {
     console.log("Congragulation , you win");
 }
-else {
-    console.log("Please Try Agian");
+else if (result != answers.number) {
+    console.log(`You lost the game! Correct number was ${result} `);
+    console.log("-------------------- loser --------------------");
 }
